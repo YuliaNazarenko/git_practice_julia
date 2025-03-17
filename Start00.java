720c980b5dbbfb4a97d3cac803281b2505c056d2
YuliaNazarenko <yuliya.nazarenko.2023@gmail.com>
2025-03-17 15:23:04 +0100

    Changes for branch4

--- Start00.java
+++ Start00.java
@@ -3,5 +3,6 @@ public class Start00 {
     public static void main(String[] args) {
         System.out.println("Hello World!!!Hello World!");
         System.out.println("Change");
+        System.out.println("Changes for branch4");
     }
 }
